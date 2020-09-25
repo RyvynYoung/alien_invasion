a035c49938d58e49749406c7217a737eb24f7e12
RyvynYoung <ryvyn.young@gmail.com>
2020-09-25 16:52:29 -0500

    bullets now eliminate aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,11 +75,24 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullets and aliens that have collided"""
+        # remove and bullets and aliens that have collided
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """Check if fleet is at an edge then update the positions of all aliens in the fleet"""
         self._check_fleet_edges()        
         self.aliens.update()
+        # Look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
