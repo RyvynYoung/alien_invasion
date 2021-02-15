b395153eeb154788e9c73d56c899a62ffb5baa42
RyvynYoung <ryvyn.young@gmail.com>
2021-02-15 14:25:18 -0600

    display scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,19 @@ import pygame.font
 
 class Scoreboard:
     """A class to report scoring information"""
+
+    def __init__(self, ai_game):
+        """Initialize scorekeeping attributes"""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+        # Font settings for scoring information
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+        # Prepare the initial score image
+        self.prep_score()
+        
     def show_score(self):
         """Draw score to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
@@ -15,17 +28,6 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
-    def __init__(self, ai_game):
-        """Initialize scorekeeping attributes"""
-        self.screen = ai_game.screen
-        self.screen_rect = self.screen.get_rect()
-        self.settings = ai_game.settings
-        self.stats = ai_game.stats
-        # Font settings for scoring information
-        self.text_color = (30, 30, 30)
-        self.font = pygame.font.SysFont(None, 48)
-        # Prepare the initial score image
-        self.prep_score()
 
 
 
